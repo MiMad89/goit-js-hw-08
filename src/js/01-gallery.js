30708a64080ee966aa02c0429736a75d76f953cf
MiMad89 <michal9977@gmail.com>
2023-03-31 23:32:25 +0200

    Update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,47 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-// Change code below this line
+
+const galleryEl = document.querySelector('.gallery');
+
+galleryItems.forEach(img => {
+  galleryEl.insertAdjacentHTML(
+    'afterbegin',
+    `<div class="gallery__item">
+  <a class="gallery__link" href="${img.original}">
+    <img
+      class="gallery__image"
+      src="${img.preview}"
+      data-source="${img.original}"
+      alt="${img.description}"
+    />
+  </a>
+</div>`
+  );
+});
+
+galleryEl.addEventListener('click', event => {
+  event.preventDefault();
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}"/>`,
+    {
+      onShow: instance => {
+        galleryEl.addEventListener('keydown', event => {
+          if (event.key === 'Escape') {
+            instance.close();
+          }
+        });
+      },
+    }
+  );
+  instance.show();
+});
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'src',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+});
+
 
 console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const videoEl = document.querySelector('#vimeo-player');
+const player = new Player(videoEl);
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const savePlayerTime = throttle(
+  data => localStorage.setItem(STORAGE_KEY, data.seconds),
+  1000
+);
+
+player.setCurrentTime(localStorage.getItem(STORAGE_KEY)).then();
+player.on('timeupdate', savePlayerTime);
